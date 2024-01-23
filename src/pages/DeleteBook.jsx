36ac3e04aa931b9d087b2907382fd97a4dc0f6ff
Lakshmi NArayanan <lakshminarayanandd@gmail.com>
2024-01-23 19:16:18 +0530

    second commit

---
+++ src/pages/DeleteBook.jsx
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import {useNavigate, useParams} from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+
+const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios.delete(`${import.meta.env.VITE_PUBLIC_BACKEND_API}/books/${id}`)
+    .then(() => {
+      setLoading(false);
+      enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
+      navigate('/');
+    })
+    .catch((error) => {
+      setLoading(false);
+      //alert('Something went wrong. Please check console');
+      enqueueSnackbar('Error', { variant: 'error' });
+      console.log(error);
+    })
+  }
+
+  return (
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Delete Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col item-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
+        <h3 className='text-2xl'>Would you like to confirm the deletion of this book?</h3>
+
+        <button 
+        className='p-4 bg-red-600 text-white m-8 w-full max-w-[400px] mx-auto'
+        onClick={handleDeleteBook}
+        >
+          Yes, proceed with the deletion
+        </button>
+
+
+      </div>
+
+    </div>
+  )
+}
+
+export default DeleteBook
